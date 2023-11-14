70e8c58548585745796615ae5b9db2943653e187
Paulo123 <paulinho17xd@gmail.com>
2023-11-13 20:06:14 -0500

    Actualizacion

--- src/main/java/HadoopMini/Tupla.java
+++ src/main/java/HadoopMini/Tupla.java
@@ -8,7 +8,7 @@ package HadoopMini;
  */
 public class Tupla {
     
-     private Object clave; //son tipo Object para que pueda ser cualquier tipo de dato
+    private Object clave; //son tipo Object para que pueda ser cualquier tipo de dato
     private Object valor; //son tipo Object para que pueda ser cualquier tipo de dato
 
     public Tupla(Object clave, Object valor) { // Constructor
